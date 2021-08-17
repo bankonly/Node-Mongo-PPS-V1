16956a26bc3503c89cc97705b3cb8dc1e359e8bd
bankonly <for21bn01@gmail.com>
2021-08-17 09:46:36 +0700

    asd

--- src/middlewares/instructor.middleware.js
+++ src/middlewares/instructor.middleware.js
@@ -3,10 +3,10 @@ const { _ } = require("../utils/common-func");
 const InstructorModel = require("codian-academy-model/models/instructor.model");
 const Mongo = require("../utils/mongo-query");
 
-const InstructorMiddleware = Catcher(async (req, res, next) => {
+const InstructorMiddleware = Catcher(async(req, res, next) => {
     const user_id = req.user._id
     const found_instructor = await Mongo.find(InstructorModel, { condition: { user_id }, error_code: 401, throw_error: true, many: false })
-    req.body.instructor = found_instructor
+    req.instructor = found_instructor
     next()
 })
 
